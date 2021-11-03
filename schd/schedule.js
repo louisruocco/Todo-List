60bca872e30ee1584144f1a1c0729bc7d42d4b97
Louis Ruocco <louisruocco1@gmail.com>
2021-11-03 20:17:55 +0000

    Chanmged schedule code back to before

--- schd/schedule.js
+++ schd/schedule.js
@@ -10,31 +10,33 @@ const sendEmail = schedule.scheduleJob("0 */2 * * *", () => {
         if(err){
             return console.log(err)
         } else {
-            const transporter = nodemailer.createTransport({
-                service: "gmail", 
-                auth: {
-                    user: process.env.EMAIL,
-                    pass: process.env.PASS
-                }
-            })
-
-            const options = {
-                from: process.env.EMAIL,
-                to: email.email,
-                subject: `TODO ALERT: ${email.todo}`,
-                html: `
-                <h1>${email.todo} is outstanding</h1>
-
-                <b>Make Sure to tick it off the list when completed!</b>
-                `
-            };
-
-            transporter.sendMail(options, (err, info) => {
-                if(err){
-                    return console.log(err);
-                } else {
-                    console.log("sent: " + info.response);
-                }
+            email.forEach(todo => {
+                const transporter = nodemailer.createTransport({
+                    service: "gmail", 
+                    auth: {
+                        user: process.env.EMAIL,
+                        pass: process.env.PASS
+                    }
+                })
+    
+                const options = {
+                    from: process.env.EMAIL,
+                    to: todo.email,
+                    subject: `TODO ALERT: ${todo.todo}`,
+                    html: `
+                    <h1>${todo.todo} is outstanding</h1>
+    
+                    <b>Make Sure to tick it off the list when completed!</b>
+                    `
+                };
+    
+                transporter.sendMail(options, (err, info) => {
+                    if(err){
+                        return console.log(err);
+                    } else {
+                        console.log("sent: " + info.response);
+                    }
+                })
             })
         }
     })
