72a613d58f6e9c8f758e98ec00127bf270880468
Louis Ruocco <louisruocco1@gmail.com>
2021-11-03 19:18:51 +0000

    Email sending two of each email, removed loop to see if that fixes it

--- schd/schedule.js
+++ schd/schedule.js
@@ -10,33 +10,31 @@ const sendEmail = schedule.scheduleJob("0 */2 * * *", () => {
         if(err){
             return console.log(err)
         } else {
-            email.forEach(todo => {
-                const transporter = nodemailer.createTransport({
-                    service: "gmail", 
-                    auth: {
-                        user: process.env.EMAIL,
-                        pass: process.env.PASS
-                    }
-                })
+            const transporter = nodemailer.createTransport({
+                service: "gmail", 
+                auth: {
+                    user: process.env.EMAIL,
+                    pass: process.env.PASS
+                }
+            })
 
-                const options = {
-                    from: process.env.EMAIL,
-                    to: todo.email,
-                    subject: `TODO ALERT: ${todo.todo}`,
-                    html: `
-                    <h1>${todo.todo} is outstanding</h1>
+            const options = {
+                from: process.env.EMAIL,
+                to: email.email,
+                subject: `TODO ALERT: ${email.todo}`,
+                html: `
+                <h1>${email.todo} is outstanding</h1>
 
-                    <b>Make Sure to tick it off the list when completed!</b>
-                    `
-                };
+                <b>Make Sure to tick it off the list when completed!</b>
+                `
+            };
 
-                transporter.sendMail(options, (err, info) => {
-                    if(err){
-                        return console.log(err);
-                    } else {
-                        console.log("sent: " + info.response);
-                    }
-                })
+            transporter.sendMail(options, (err, info) => {
+                if(err){
+                    return console.log(err);
+                } else {
+                    console.log("sent: " + info.response);
+                }
             })
         }
     })
