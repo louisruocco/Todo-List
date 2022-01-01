7cbc8f42a287d081d89c9625cfc4c771f84e07f6
Louis Ruocco <louisruocco1@gmail.com>
2022-01-01 14:30:59 +0000

    changed email to send one with all todos on

--- schd/schedule.js
+++ schd/schedule.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const session = require("express-session");
 const schedule = require("node-schedule");
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
@@ -5,35 +7,54 @@ const db = require("../db");
 
 dotenv.config({path: "./.env"});
 
-const sendEmail = schedule.scheduleJob("0 * * * *", () => {
-    db.query("SELECT * FROM todos", (err, email) => {
+const sendEmail = schedule.scheduleJob(""0 */2 * * *", () => {
+    db.query("SELECT * FROM todos", (err, todos) => {
         if(err){
             return console.log(err)
         } else {
-            email.forEach(todo => {
-                const transporter = nodemailer.createTransport({
-                    service: "gmail", 
-                    auth: {
-                        user: process.env.EMAIL,
-                        pass: process.env.PASS
-                    }
-                })
-
-                const options = {
-                    from: process.env.EMAIL,
-                    to: todo.email,
-                    subject: todo.todo, 
-                    text: todo.todo + " " + "is outstanding"
-                };
-
-                transporter.sendMail(options, (err, info) => {
+            todos.forEach(todo => {
+                db.query("SELECT email, todo FROM todos WHERE email = ?", [todo.email], (err, item) => {
                     if(err){
                         return console.log(err);
                     } else {
-                        console.log("sent: " + info.response);
+                       item.forEach(email => {
+                            const transporter = nodemailer.createTransport({
+                                service: "gmail", 
+                                auth: {
+                                    user: process.env.EMAIL,
+                                    pass: process.env.PASS
+                                }
+                            })
+                
+                            const items = item.map(todo => {
+                                return `<li>${todo.todo}</li>`
+                            })
+                
+                            const html = `
+                                <h1>Outstanding Todos:</h1>
+                                <ul>
+                                    ${items.join("")}
+                                </ul>
+                            `
+                            
+                            const options = {
+                                from: process.env.EMAIL,
+                                to: email.email,
+                                subject: "TODO LIST ALERTS", 
+                                html,
+                            };
+                
+                            transporter.sendMail(options, (err, info) => {
+                                if(err){
+                                    return console.log(err);
+                                } else {
+                                    console.log("sent: " + info.response);
+                                }
+                            })
+                       }) 
                     }
                 })
             })
-        }
+        }   
     })
 })
