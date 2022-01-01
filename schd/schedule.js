f7bc05da106ce77dab0b10f2c359b8da84aaa157
Louis Ruocco <louisruocco1@gmail.com>
2022-01-01 18:45:19 +0000

    changed schedukle to send just one email per email address instead of an email per entry in db

--- schd/schedule.js
+++ schd/schedule.js
@@ -8,53 +8,51 @@ const db = require("../db");
 dotenv.config({path: "./.env"});
 
 const sendEmail = schedule.scheduleJob("0 */2 * * *", () => {
-    db.query("SELECT * FROM todos", (err, todos) => {
+    db.query("SELECT DISTINCT email FROM todos", (err, email) => {
         if(err){
             return console.log(err)
         } else {
-            todos.forEach(todo => {
-                db.query("SELECT email, todo FROM todos WHERE email = ?", [todo.email], (err, item) => {
+            email.forEach(todo => {
+                return db.query("SELECT email, todo FROM todos WHERE email = ?", [todo.email], (err, item) => {
                     if(err){
                         return console.log(err);
                     } else {
-                       item.forEach(email => {
-                            const transporter = nodemailer.createTransport({
-                                service: "gmail", 
-                                auth: {
-                                    user: process.env.EMAIL,
-                                    pass: process.env.PASS
-                                }
-                            })
-                
-                            const items = item.map(todo => {
-                                return `<li>${todo.todo}</li>`
-                            })
-                
-                            const html = `
-                                <h1>Outstanding Todos:</h1>
-                                <ul>
-                                    ${items.join("")}
-                                </ul>
-                            `
-                            
-                            const options = {
-                                from: process.env.EMAIL,
-                                to: email.email,
-                                subject: "TODO LIST ALERTS", 
-                                html,
-                            };
-                
-                            transporter.sendMail(options, (err, info) => {
-                                if(err){
-                                    return console.log(err);
-                                } else {
-                                    console.log("sent: " + info.response);
-                                }
-                            })
-                       }) 
+                        const transporter = nodemailer.createTransport({
+                            service: "gmail", 
+                            auth: {
+                                user: process.env.EMAIL,
+                                pass: process.env.PASS
+                            }
+                        })
+            
+                        const items = item.map(todo => {
+                            return `<li>${todo.todo}</li>`
+                        })
+            
+                        const html = `
+                            <h1>Outstanding Todos:</h1>
+                            <ul>
+                                ${items.join("")}
+                            </ul>
+                        `
+                        
+                        const options = {
+                            from: process.env.EMAIL,
+                            to: todo.email,
+                            subject: "TODO LIST ALERTS", 
+                            html,
+                        };
+            
+                        transporter.sendMail(options, (err, info) => {
+                            if(err){
+                                return console.log(err);
+                            } else {
+                                console.log("sent: " + info.response);
+                            }
+                        })
                     }
                 })
-            })
+            })            
         }   
     })
 })
